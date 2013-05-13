8b4659493486ee84acdc8fc85b89b8b9985859c4
Matthew <chefmattrock@gmail.com>
2013-05-12 23:20:21 -0400

    added proper greeting depending on time of day.

--- lib/goodmorning.py
+++ lib/goodmorning.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
-import pyttsx
 
-engine = pyttsx.init()
-engine.say("Good morning mister stein")
-engine.runAndWait()
+"""Good morning Mr Stein. You asked me to wake you up today at 8:00.  It's currently a lovely day, """
+
+import pyttsx, datetime, time
+
+def run():
+    now = getTime()
+    greeting = properGreeting(now)
+    print greeting
+    engine = pyttsx.init()
+    engine.setProperty('rate', 160)
+    engine.say("Good")
+    engine.say(greeting)
+    engine.say("mister stein")
+    engine.runAndWait()
+
+    
+def properGreeting(time):
+    if int(time[0:2]) < 12:
+	greeting = 'morning'
+    elif int(time[0:2]) < 17:
+	greeting = 'afternoon'
+    else:
+	greeting = 'evening'
+    return greeting
+
+def getTime():
+   now = datetime.datetime.fromtimestamp(int(time.time())).strftime('%H:%M')
+   return now
+
+if __name__ == '__main__':
+    run()
